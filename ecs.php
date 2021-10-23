e19dafbbc717d1ce9c04b05ae09d3077c29546ae
SelrahcD <c.desneuf@gmail.com>
2021-10-23 11:03:35 +0200

    Add utils namespace

--- ecs.php
+++ ecs.php
@@ -13,7 +13,7 @@ return static function (ContainerConfigurator $containerConfigurator): void {
 
     $parameters = $containerConfigurator->parameters();
 
-    $parameters->set(Option::PATHS, [__DIR__ . '/src', __DIR__ . '/tests']);
+    $parameters->set(Option::PATHS, [__DIR__ . '/src', __DIR__ . '/utils',  __DIR__ . '/tests']);
 
     $containerConfigurator->import(SetList::PSR_12);
 
