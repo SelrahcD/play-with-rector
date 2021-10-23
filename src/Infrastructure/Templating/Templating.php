865a2ac500eebca3cc8218ae5da7534392771362
SelrahcD <c.desneuf@gmail.com>
2021-10-23 11:03:48 +0200

    Add templating interface

---
+++ src/Infrastructure/Templating/Templating.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Infrastructure\Templating;
+
+use Symfony\Component\HttpFoundation\Response;
+
+interface Templating
+{
+    public function render(string $templateFile, array $parameters): Response;
+}
