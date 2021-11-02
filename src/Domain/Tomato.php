309c271adf55862fcb610c55fc568ab669722de1
SelrahcD <c.desneuf@gmail.com>
2021-11-02 20:20:02 +0100

    Try to generate Glossary

---
+++ src/Domain/Tomato.php
@@ -0,0 +1,17 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Domain;
+
+/**
+ *
+ * A tomato is a fruit, but sometimes is a vegetable
+ *
+ * @description A tomato is a fruit, but sometimes is a vegetable
+ * @AggregateRoot
+ */
+final class Tomato
+{
+
+}
